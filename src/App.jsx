a7e6bfcfb176d24e16f455ae2437b060ab6a04e4
JavierChacon <alejandro.chr@outlook.es>
2022-09-05 22:51:33 -0400

    functional part of the machine

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,60 @@ import { useEffect, useState } from 'react';
 import './App.css'
 
 function App() {
+  
+  const [weather, setWeather] = useState({})
+  
+  const [isCelsius, setIsCelsius] = useState(true)
 
+  const [isFarenheit, setIsFarenheit] = useState(true)
+  
+  const changeUnits = () => {
+    
+    setIsFarenheit(!isCelsius)
+    
+    setIsCelsius(!isFarenheit)
+  }
+
+
+  
   useEffect(() => {
     
     function success(pos) {
+      
       const crd = pos.coords;
 
       axios
-      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=2ee5a48d04f073af2c82d23c4af1665e`)
-      .then((res) => console.log(res.data))
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&appid=2ee5a48d04f073af2c82d23c4af1665e&units=${isCelsius ? "metric" : "imperial"}`)
+      .then((res) => setWeather(res.data))
     }
     
-    const error = () => 'Please allow the ubication to use the app'
+    const error = () => alert('Please allow the ubication to use the app')
     
     navigator.geolocation.getCurrentPosition(success, error)
 
-  }, [])
+  }, [isCelsius])
+
+  const image = `http://openweathermap.org/img/wn/${weather.weather?.[0]?.icon}@2x.png`
+  
 
 
   return (
     <div className="App">
-     <h1>Pollito frito</h1>
+     <div className="card">
+      <h2>The Weather Machine</h2>
+      <h3>{weather.sys?.country}, {weather.name}</h3>
+      <div className='info'>
+        <img src={image} className='icon' />
+        <ul>
+          <li>{weather.weather?.[0]?.description}</li>
+          <li>Temperature: {weather.main?.temp}</li>
+          <li>Wind Speed: {weather.wind?.speed}</li>
+          <li>Clouds:{weather.clouds?.all}%</li>
+          <li>Pressure:{weather.main?.pressure} </li>
+        </ul>
+      </div>
+      <button onClick={changeUnits}>Change units to {isCelsius ? "imperial" : "metric"}</button>
+     </div>
     </div>
   )
 }
